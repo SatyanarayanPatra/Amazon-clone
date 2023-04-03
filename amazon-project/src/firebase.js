56bd991a9a1f420b3c68f651dc115c4bdb0b2d15
satyanarayanPatra <69004750+SatyanarayanPatra@users.noreply.github.com>
2023-04-04 02:37:51 +0530

    vite & firebase setup

---
+++ amazon-project/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC8YwZ7-FXZx9zLIWO617roJuNK17jX06g',
+  authDomain: 'amzn-fs-react-vite.firebaseapp.com',
+  projectId: 'amzn-fs-react-vite',
+  storageBucket: 'amzn-fs-react-vite.appspot.com',
+  messagingSenderId: '444366800932',
+  appId: '1:444366800932:web:37a5fa3d69f671f06b70b1',
+  measurementId: 'G-7VK7CWVXX8',
+};
